240efb71061ceb0d5b25719cf7b4b1bde7cb514b
ns-112 <nps007@latech.edu>
2026-03-14 17:49:32 -0500

    movement adjustments

--- Assets/Player Assets/Scripts/Movement.cs
+++ Assets/Player Assets/Scripts/Movement.cs
@@ -6,7 +6,8 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     
-    
+    public float rotationSpeed = 10.5f;
+
     private InputActionAsset inputActions;
     private Rigidbody rb;
 
@@ -24,6 +25,7 @@ public class Movement : MonoBehaviour
         fire = inputActions["Fire"];
 
         rb = GetComponent<Rigidbody>();
+        
     }
 
     
@@ -33,12 +35,41 @@ public class Movement : MonoBehaviour
         float y = upDown.ReadValue<float>();
 
         Vector2 movement = new Vector2(x, y);
-
         if (fire.WasPressedThisFrame())
+                {
+                    Debug.Log("Fire pressed");
+                }
+
+
+        Vector2 mouse = Mouse.current.position.ReadValue();
+
+        Ray ray = Camera.main.ScreenPointToRay(mouse);
+        Plane plane = new(Vector3.up, transform.position);
+
+        if (plane.Raycast(ray, out float distance))
         {
-            Debug.Log("Fire pressed");
+            Vector3 mousePos = ray.GetPoint(distance);
+
+            //convert from screen to world pos
+            Vector3 targetDirection = mousePos - transform.position;
+            targetDirection.y = 0; //ignore y
+
+            if (targetDirection.sqrMagnitude > 0.001f) // avoid zero-length vectors
+            {
+                //target rotation
+                Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+
+                //Smooth lerp towards mouse pos
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,   
+                    targetRotation,          
+                    rotationSpeed * Time.deltaTime 
+                );
+            }
         }
-        rb.AddForce(new Vector3(x, 0, y));
+
+        
+        rb.AddForce(new Vector3(x*2, 0, y*2));
         Debug.Log(movement);
     }
 }
